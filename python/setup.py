18e5b2e0b27f249a38d3fa2321d6c64111b06074
Mathieu Lecarme <mathieu@garambrogne.net>
2010-10-30 23:08:16 +0200

    ip2db util

--- python/setup.py
+++ python/setup.py
@@ -1,5 +1,8 @@
 from distutils.core import setup
 setup(name='ip2something',
-      version='0.1',
+      version='0.2',
+      package_dir={'': 'src'},
+      url='http://github.com/athoune/ip2something',
+      scripts=['bin/ip2db'],
       py_modules=['ip2something'],
       )
