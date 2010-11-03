aaa8c5c8a2e8f64aeef3e57b59abf8f8ac25463d
Mathieu Lecarme <mathieu@garambrogne.net>
2010-11-03 22:47:19 +0100

    it just works

--- php/class.ip2something.php
+++ php/class.ip2something.php
@@ -8,6 +8,7 @@ class IP2Something {
 	public function __construct($path) {
 		$this->keys = fopen("$path/ip.keys", 'r');
 		$this->datas = fopen("$path/ip.data", 'r');
+		$this->length = filesize("$path/ip.keys") / 10;
 	}
 	function getKey($poz) {
 		fseek($this->keys, $poz * 10);
@@ -18,5 +19,53 @@ class IP2Something {
 		$values = unpack('Npoz/nsize', fread($this->keys, 6));
 		fseek($this->datas, $values['poz']);
 		return fread($this->datas, $values['size']);
+	}
+	function search($ip) {
+		$k = pack('N', ip2long($ip));
+		$cpt = 0;
+		$high = $this->length;
+		$low = 0;
+		while(true) {
+			$cpt++;
+			$pif = floor(($high + $low) / 2);
+			$kpif = $this->getKey($pif);
+			if($kpif == $k || ($pif > 1 && $this->getKey($pif-1) < $k && $kpif > $k )) {
+				$datas = explode('|', $this->getData($pif-1));
+				if(sizeof($datas) == 9) {
+					return array(
+					'country_code' => $datas[0],
+					'country_name' => $datas[1],
+					'region_code'  => $datas[2],
+					'region_name'  => $datas[3],
+					'city'         => $datas[4],
+					'zipcode'      => $datas[5],
+					'latitude'     => $datas[6],
+					'longitude'    => $datas[7],
+					'metrocode'    => $datas[8]
+					);
+				}
+				return $datas;
+			}
+			if($kpif > $k) {
+				$high = $pif;
+			} else {
+				$low = $pif;
+			}
+		}
+		/*
+		cpt = 0
+		high = self.length
+		low = 0
+		while True:
+			cpt += 1
+			pif = (high+low) / 2
+			#print pif
+			if self.getKey(pif) == k or (pif > 1 and self.getKey(pif-1) < k and self.getKey(pif) > k):
+				return self.toDict(self.getData(pif-1).split('|')) #socket.inet_ntoa(self.getKey(pif-1))
+			if self.getKey(pif) > k :
+				high = pif
+			else:
+				low = pif
+		*/
 	}
 }
--- php/test.ip2something.php
+++ php/test.ip2something.php
@@ -6,7 +6,11 @@ class IpTest extends PHPUnit_Framework_TestCase {
 		$this->ip = new IP2Something($_ENV['HOME'] . '/.ip2something');
 	}
 	public function testKey() {
-		var_dump($this->ip->getKey(0));
-		var_dump($this->ip->getData(0));
+		$d = explode('|', $this->ip->getData(0));
+		$this->assertEquals('RD', $d[0]);
+	}
+	function testSimple() {
+		$s = $this->ip->search('82.227.122.98');
+		$this->assertEquals('Vincennes', $s['city']);
 	}
 }
