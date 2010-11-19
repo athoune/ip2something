a12e9e4037d57aff75b603ce6e5b7f8c0e1ec5ad
Mathieu Lecarme <mathieu@garambrogne.net>
2010-11-19 23:34:12 +0100

    ip 2 pipe

--- python/setup.py
+++ python/setup.py
@@ -3,6 +3,6 @@ setup(name='ip2something',
       version='0.2',
       package_dir={'': 'src'},
       url='http://github.com/athoune/ip2something',
-      scripts=['bin/ip2db'],
+      scripts=['bin/ip2db', 'bin/ip2pipe'],
       py_modules=['ip2something'],
       )
