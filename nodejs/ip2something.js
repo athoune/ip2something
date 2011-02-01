40e242d10c792a24db64d322dc3f48347f3002da
Mathieu Lecarme <mathieu@garambrogne.net>
2011-02-01 20:09:52 +0100

    key

--- nodejs/ip2something.js
+++ nodejs/ip2something.js
@@ -9,8 +9,15 @@ var Index = function(folder) {
 	this.datas = fs.openSync(f+"/ip.data", 'r');
 };
 
-Index.prototype.key = function(poz) {
-	
+Index.prototype.key = function(poz, cb) {
+	//self.keys.seek(poz * 10)
+	//return self.keys.read(4)
+	var buffer = new Buffer(4);
+	var i = this;
+	fs.read(this.keys, buffer, 0, 4, poz*10, function(err, data) {
+		if (err) throw err;
+		cb.call(i, buffer);
+	});
 }
 
 Index.prototype.data = function(poz) {
@@ -19,3 +26,6 @@ Index.prototype.data = function(poz) {
 
 
 var i = new Index();
+i.key(42, function(key) {
+	console.log(key);
+})
