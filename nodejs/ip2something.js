a5d79b0e809b8bf303b82ac742f781f1b3caf1d5
Mathieu Lecarme <mathieu@garambrogne.net>
2011-02-01 23:06:54 +0100

    as int and reading the index

--- nodejs/ip2something.js
+++ nodejs/ip2something.js
@@ -1,5 +1,8 @@
-var BinaryParser = require('./binary_parser'),
-	util = require('util'),
+/*
+encoding stolen from https://github.com/danielgtaylor/qtfaststart.js/blob/master/qtfaststart.js
+*/
+
+var util = require('util'),
 //	process = require('process'),
 	fs = require('fs');
 
@@ -11,29 +14,46 @@ var Index = function(folder) {
 
 Index.prototype.key = function(poz, cb) {
 	var buffer = new Buffer(4);
-	var i = this;
+	var idx = this;
 	fs.read(this.keys, buffer, 0, 4, poz*10, function(err, data) {
 		if (err) throw err;
-		cb.call(i, buffer);
+		cb.call(idx, buffer);
 	});
-}
+};
+
+/*
+    Make a string method for converting a four character string into a big
+    endian 32-bit unsigned integer.
+*/
+Buffer.prototype.asUInt32BE = function() {
+	return (this[0] << 24) + (this[1] << 16) + (this[2] << 8) + this[3];
+};
+
+Buffer.prototype.asShortBE = function() {
+	return  (this[0] << 8) + this[1];
+};
 
 Index.prototype.data = function(poz, cb) {
 	/*self.keys.seek(poz * 10 + 4)
 	poz, size = struct.unpack('!LH', self.keys.read(6))
 	self.datas.seek(poz)
 	return self.datas.read(size)*/
+	var idx = this;
 	var buffer = new Buffer(6);
 	fs.read(this.keys, buffer, 0, 6, poz*10+4, function(err, data) {
 		if (err) throw err;
-		console.log(buffer.to_float());
+		var dataPoz = buffer.slice(0,4).asUInt32BE();
+		var dataSize = buffer.slice(4,6).asShortBE();
+		var dataBuffer = new Buffer(dataSize);
+		fs.read(idx.datas, dataBuffer, 0, dataSize, dataPoz, function(err, data) {
+			cb.call(idx, dataBuffer.toString('utf8'));
+		});
 	});
-}
+};
 
 
 var i = new Index();
-i.key(42, function(key) {
-	console.log(key);
-})
 
-i.data(42);
+i.data(43, function(data) {
+	console.log(data);
+});
