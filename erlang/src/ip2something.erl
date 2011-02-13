5c4a443e56e5a443cc4668d6111f6aba1b2956e7
Mathieu Lecarme <mathieu@garambrogne.net>
2011-02-14 00:06:55 +0100

    parsing csv line

--- erlang/src/ip2something.erl
+++ erlang/src/ip2something.erl
@@ -6,14 +6,24 @@
 
 read_csv(Path) ->
     {ok, F} = file:open(Path, [read, {encoding,latin1}]),
+    file:read_line(F), %%I don't care about the first line
     read_line(F),
     ok.
 
-read_line(Fd) ->    case file:read_line(Fd) of 
+read_line(Fd) ->
+    case file:read_line(Fd) of 
     {ok, Data} ->
-        Tokens = string:tokens(Data, ";"),
-        io:format("line ~p", [Tokens]),
+        [Ip|Tokens] = lists:map(fun(Dirty) ->
+            case Dirty of
+                "" -> "";
+                "\n" -> "";
+                _ -> string:substr(Dirty, 2, string:len(Dirty) -2)
+            end
+        end, string:tokens(string:substr(Data, 1, string:len(Data) -2), ";")),
+        {Ipv, _} = string:to_integer(Ip),
+        io:format("line ~B : ~p\n", [Ipv, Tokens]),
         read_line(Fd);
     eof -> ok;
     {error, Reason} -> {stop, Reason}
 end.
+    
