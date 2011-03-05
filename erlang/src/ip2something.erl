706d11a886a5d45b3d5a0b3013a47f668b493523
Mathieu Lecarme <mathieu@garambrogne.net>
2011-03-05 22:31:34 +0100

    feeding dets table

--- erlang/src/ip2something.erl
+++ erlang/src/ip2something.erl
@@ -15,16 +15,19 @@ read_csv(Path) ->
     {ok, F} = file:open(Path, [read, {encoding,latin1}]),
     io:get_line(F, ""), %%I don't care about the first line
     {ok, Index} = file:open("./index.db", [write]),
-    read_line(F, Index).
+    {ok, Datas} = dets:open_file(ips, [
+        {file,"./data.db"}
+        ]),
+    read_line(F, Index, Datas).
 
-read_line(Fd, Index) ->
+read_line(Fd, Index, Datas) ->
     case io:get_line(Fd, "") of 
     eof -> 
         file:close(Index),
         ok;
-    %{error, Reason} ->
-    %    io:format("Erreur: ~w", [Reason]),
-    %    {stop, Reason};
+    {error, Reason} ->
+        io:format("Erreur: ~w", [Reason]),
+        {stop, Reason};
     Data ->
         %io:format("~p ~n ~p ~n", [Data, string:tokens(string:substr(Data, 1, string:len(Data) -2), ";")]),
         [Ip|Tokens] = lists:map(fun(Dirty) ->
@@ -38,8 +41,9 @@ read_line(Fd, Index) ->
             string:tokens(string:substr(Data, 1, string:len(Data) -2), ";")),
         {Ipv, _} = string:to_integer(Ip),
         file:write(Index, <<Ipv:32/unsigned-integer>>),
+        dets:insert_new(Datas, {Ip, Tokens}),
         %io:format("line ~B : ~p\n", [Ipv, Tokens]),
-        read_line(Fd, Index)
+        read_line(Fd, Index, Datas)
     end.
 
 str_ip_to_int(Ip) ->
