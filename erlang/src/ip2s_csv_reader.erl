a42dfb607b49ba4369fd65809ff86a4f39e101d9
Mathieu Lecarme <mathieu@garambrogne.net>
2011-03-07 00:06:42 +0100

    index as binary

--- erlang/src/ip2s_csv_reader.erl
+++ erlang/src/ip2s_csv_reader.erl
@@ -8,19 +8,22 @@
 ]).
 
 %% Read CSV source and build index
+% [TODO] using dets:init_table/2
 read_csv(Path) ->
     {ok, F} = file:open(Path, [read, {encoding,latin1}]),
     io:get_line(F, ""), %%I don't care about the first line
     {ok, Index} = file:open("./index.db", [write]),
     {ok, Datas} = dets:open_file(?IP2S_DATA, [
-        {file,"./data.db"}
+        {file,"./data.db"},
+        {min_no_slots, 150000}
         ]),
     read_line(F, Index, Datas).
 
 read_line(Fd, Index, Datas) ->
     case io:get_line(Fd, "") of 
     eof -> 
-        file:close(Index),
+        ok = file:close(Index),
+        ok = dets:close(?IP2S_DATA),
         ok;
     {error, Reason} ->
         io:format("Erreur: ~w", [Reason]),
@@ -37,8 +40,9 @@ read_line(Fd, Index, Datas) ->
             end,
             string:tokens(string:substr(Data, 1, string:len(Data) -2), ";")),
         {Ipv, _} = string:to_integer(Ip),
-        file:write(Index, <<Ipv:32/unsigned-integer>>),
-        dets:insert_new(Datas, {Ip, Tokens}),
+        Ipb = <<Ipv:32/unsigned-integer>>,
+        file:write(Index, Ipb),
+        dets:insert_new(Datas, {Ipb, Tokens}),
         %io:format("line ~B : ~p\n", [Ipv, Tokens]),
         read_line(Fd, Index, Datas)
     end.
