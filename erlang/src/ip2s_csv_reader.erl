738d82d5e93a8f380955804e5d0ef81d868cf7dc
Mathieu Lecarme <mathieu@garambrogne.net>
2011-03-07 16:21:46 +0100

    csv split handles empty columns

--- erlang/src/ip2s_csv_reader.erl
+++ erlang/src/ip2s_csv_reader.erl
@@ -7,6 +7,10 @@
     read_csv/1
 ]).
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 %% Read CSV source and build index
 % [TODO] using dets:init_table/2
 read_csv(Path) ->
@@ -15,7 +19,7 @@ read_csv(Path) ->
     {ok, Index} = file:open("./index.db", [write]),
     {ok, Datas} = dets:open_file(?IP2S_DATA, [
         {file,"./data.db"},
-        {min_no_slots, 150000}
+        {min_no_slots, 4047600}
         ]),
     read_line(F, Index, Datas).
 
@@ -30,20 +34,29 @@ read_line(Fd, Index, Datas) ->
         {stop, Reason};
     Data ->
         %io:format("~p ~n ~p ~n", [Data, string:tokens(string:substr(Data, 1, string:len(Data) -2), ";")]),
-        [Ip|Tokens] = lists:map(fun(Dirty) ->
-                %io:format("~p~n", [Dirty]),
-                case Dirty of
-                    "" -> "";
-                    "\n" -> "";
-                    _ -> string:substr(Dirty, 2, string:len(Dirty) -2)
-                end
-            end,
-            string:tokens(string:substr(Data, 1, string:len(Data) -2), ";")),
-        {Ipv, _} = string:to_integer(Ip),
-        Ipb = <<Ipv:32/unsigned-integer>>,
+        {ok, Ipb, Tokens } = parse_line(Data),
         file:write(Index, Ipb),
         dets:insert_new(Datas, {Ipb, Tokens}),
         %io:format("line ~B : ~p\n", [Ipv, Tokens]),
         read_line(Fd, Index, Datas)
     end.
 
+parse_line(Line) ->
+    [Ip|Tokens] = lists:map(fun(Dirty) ->
+            %io:format("~p~n", [Dirty]),
+            case Dirty of
+                "" -> "";
+                "\n" -> "";
+                _ -> string:substr(Dirty, 2, string:len(Dirty) -2)
+            end
+        end,
+        re:split(string:substr(Line, 1, string:len(Line) -2), ";",[{return,list}])),
+    {Ipv, _} = string:to_integer(Ip),
+    Ipb = <<Ipv:32/unsigned-integer>>,
+    {ok, Ipb, Tokens}.
+
+-ifdef(TEST).
+% [TODO] more complete unit test
+parse_line_test() ->
+    parse_line("\"3651660428\";\"FR\";\"France\";\"A8\";\"Ile-de-France\";\"Vincennes\";;\"48.85\";\"2.4333\";").
+-endif.
